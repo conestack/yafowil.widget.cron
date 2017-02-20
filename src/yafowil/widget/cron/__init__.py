eaeb7dc41207fdc129ee723dacf257159cb3d5c6
Johannes Raggam <thetetet@gmail.com>
2017-02-20 15:57:28 +0100

    moving forward

--- src/yafowil/widget/cron/__init__.py
+++ src/yafowil/widget/cron/__init__.py
@@ -5,24 +5,16 @@ import os
 
 
 resourcedir = os.path.join(os.path.dirname(__file__), 'resources')
+
 js = [{
     'group': 'yafowil.widget.cron.common',
     'resource': 'widget.js',
     'order': 21,
 }]
+
 default_css = [{
     'group': 'yafowil.widget.cron.common',
     'resource': 'widget.css',
-    'order': 21,
-}]
-bootstrap_css = [{
-    'group': 'yafowil.widget.cron.common',
-    'resource': 'widget-bootstrap.css',
-    'order': 21,
-}]
-plone5_css = [{
-    'group': 'yafowil.widget.cron.common',
-    'resource': 'widget-bootstrap.css',
     'order': 21,
 }]
 
@@ -30,9 +22,10 @@ plone5_css = [{
 @entry_point(order=10)
 def register():
     import widget
-    factory.register_theme('default', 'yafowil.widget.cron',
-                           resourcedir, js=js, css=default_css)
-    factory.register_theme('bootstrap', 'yafowil.widget.cron',
-                           resourcedir, js=js, css=bootstrap_css)
-    factory.register_theme('plone5', 'yafowil.widget.cron',
-                           resourcedir, js=js, css=plone5_css)
+    factory.register_theme(
+        'default',
+        'yafowil.widget.cron',
+        resourcedir,
+        js=js,
+        css=default_css
+    )
--- src/yafowil/widget/cron/example.py
+++ src/yafowil/widget/cron/example.py
@@ -1,108 +1,29 @@
 # -*- coding: utf-8 -*-
 from yafowil.base import factory
 
+DOC_CRON = """
+Crontab widget
+--------------
 
-DOC_DATE = """
-Date
-----
-
-Date input.
-
-.. code-block:: python
-
-    date = factory('#field:datetime', props={
-        'label': 'Enter date or use date picker',
-        'required': 'Date Field is required',
-        'locale': 'de',
-        'datepicker': True,
-    })
-"""
-
-
-def date_example():
-    form = factory('fieldset', name='yafowil.widget.datetime.date')
-    form['date'] = factory('#field:datetime', props={
-        'label': 'Enter date or use date picker',
-        'required': 'Date Field is required',
-        'locale': 'de',
-        'datepicker': True,
-    })
-    return {
-        'widget': form,
-        'doc': DOC_DATE,
-        'title': 'Date',
-    }
-
-
-DOC_TIME = """
-Time
-----
-
-Time input.
+Cron input.
 
 .. code-block:: python
 
-    time = factory('#field:time', props={
-        'label': 'Select time',
-        'required': 'Time Field is required',
-        'timepicker': True,
-    })
+    cron = factory('#field:cron', )
 """
 
 
-def time_example():
-    form = factory('fieldset', name='yafowil.widget.datetime.time')
-    form['time'] = factory('#field:time', props={
-        'label': 'Select time',
-        'required': 'Time Field is required',
-        'timepicker': True,
-    })
+def cron_example():
+    form = factory('fieldset', name='yafowil.widget.cron.default')
+    form['cron'] = factory('#field:cron',)
     return {
         'widget': form,
-        'doc': DOC_TIME,
-        'title': 'Time',
-    }
-
-
-DOC_DATETIME = """
-Datetime
---------
-
-Date and time input.
-
-.. code-block:: python
-
-    datetime = factory('#field:datetime', props={
-        'label': 'Enter date and time',
-        'required': 'Datetime Field is required',
-        'locale': 'de',
-        'datepicker': True,
-        'time': True,
-        'timepicker': True,
-    })
-"""
-
-
-def datetime_example():
-    form = factory('fieldset', name='yafowil.widget.datetime.datetime')
-    form['datetime'] = factory('#field:datetime', props={
-        'label': 'Enter date and time',
-        'required': 'Datetime Field is required',
-        'locale': 'de',
-        'datepicker': True,
-        'time': True,
-        'timepicker': True,
-    })
-    return {
-        'widget': form,
-        'doc': DOC_DATETIME,
-        'title': 'Datetime',
+        'doc': DOC_CRON,
+        'title': 'Cron',
     }
 
 
 def get_example():
     return [
-        date_example(),
-        time_example(),
-        datetime_example(),
+        cron_example(),
     ]
