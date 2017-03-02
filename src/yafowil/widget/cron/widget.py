cc5193e4d3952d05bbfd4da38611f1c3e20bde10
Johannes Raggam <thetetet@gmail.com>
2017-03-02 12:16:40 +0100

    cheap extractor

--- src/yafowil/widget/cron/widget.py
+++ src/yafowil/widget/cron/widget.py
@@ -6,6 +6,7 @@ from yafowil.base import fetch_value
 from yafowil.common import generic_extractor
 from yafowil.common import generic_required_extractor
 from yafowil.compound import compound_extractor
+from yafowil.compound import compound_renderer
 from yafowil.tsf import TSF
 from yafowil.utils import attr_value
 from yafowil.utils import css_managed_props
@@ -45,10 +46,20 @@ factory.doc['blueprint']['action_edit'] = UNSET
 #
 
 def cron_extractor(widget, data):
-    extracted = data.extracted
-    if extracted == UNSET or extracted == '':
-        return UNSET
-    return extracted
+
+    def _extract_part(widget, name):
+        try:
+            return data.request[widget.dottedpath + '.' + name] or UNSET
+        except KeyError:
+            return UNSET
+
+    return {
+        'minute': _extract_part(widget, 'minute'),
+        'hour': _extract_part(widget, 'hour'),
+        'dow': _extract_part(widget, 'dow'),
+        'dom': _extract_part(widget, 'dom'),
+        'month': _extract_part(widget, 'month')
+    }
 
 
 def make_cron_summary(value):
@@ -124,12 +135,12 @@ def cron_display_renderer(widget, data):
 
 factory.register(
     'cron',
-    extractors=[
-        compound_extractor,
-        cron_extractor
-    ],
+    extractors=[cron_extractor],
     edit_renderers=[cron_edit_renderer],
-    display_renderers=[cron_display_renderer]
+    display_renderers=[
+        cron_display_renderer,
+        compound_renderer
+    ]
 )
 
 factory.doc['blueprint']['cron'] = "Add-on blueprint `yafowil.widget.cron <http://github.com/bluedynamics/yafowil.widget.cron/>`_ ."  # noqa
