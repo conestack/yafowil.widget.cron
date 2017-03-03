31ee213c1aa9ba1b2a4cb0ce92158fe092a0c20b
Johannes Raggam <thetetet@gmail.com>
2017-03-03 13:28:31 +0100

    provide sample data - the namespacing in the valeu dict shoulndt be necessary - TODO

--- src/yafowil/widget/cron/example.py
+++ src/yafowil/widget/cron/example.py
@@ -9,13 +9,24 @@ Cron input.
 
 .. code-block:: python
 
-    cron = factory('#field:cron', )
+    cron = factory('#field:cron')
 """
 
 
 def cron_example():
+
+    dottedpath = 'yafowil.widget.cron.default.yafowil.widget.cron.default.cron'
+    value = {
+        dottedpath + '.minute': [0, 10, 20, 30, 40, 50],
+        dottedpath + '.hour': [0, 6, 12, 18],
+        dottedpath + '.dow': [1, 3, 5],
+        dottedpath + '.dom': [1, 15, 30],
+        dottedpath + '.month': [3, 6, 9, 12],
+        dottedpath + '.year': [2017, 2018, 2019, 2020]
+    }
+
     form = factory('fieldset', name='yafowil.widget.cron.default')
-    form['cron'] = factory('#field:cron',)
+    form['cron'] = factory('#field:cron', value=value)
     return {
         'widget': form,
         'doc': DOC_CRON,
