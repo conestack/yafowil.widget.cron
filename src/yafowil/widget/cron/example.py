cef59a1ace6dd30938696ba5a2eed1b948da20b3
Robert Niederreiter <office@squarewave.at>
2017-03-07 11:01:25 +0100

    No value in example right now

--- src/yafowil/widget/cron/example.py
+++ src/yafowil/widget/cron/example.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from node.utils import UNSET
 from yafowil.base import factory
 
 
@@ -15,14 +16,15 @@ Cron input.
 
 
 def cron_example():
-    value = {
-        'minute': '0,10,20,30,40,50',
-        'hour': '0,6,12,18',
-        'dow': '1,3,5',
-        'dom': '1,15,30',
-        'month': '3,6,9,12',
-        'year': '2017,2018,2019,2020'
-    }
+#     value = {
+#         'minute': '0,10,20,30,40,50',
+#         'hour': '0,6,12,18',
+#         'dow': '1,3,5',
+#         'dom': '1,15,30',
+#         'month': '3,6,9,12',
+#         'year': '2017,2018,2019,2020'
+#     }
+    value = UNSET
     form = factory('fieldset', name='yafowil.widget.cron.default')
     form['cron'] = factory('#field:cron', value=value)
     return {
