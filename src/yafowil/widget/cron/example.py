b6352ad7b63ecd9ea1848272752908bf0600831e
Robert Niederreiter <office@squarewave.at>
2017-05-10 23:20:15 +0200

    update example

--- src/yafowil/widget/cron/example.py
+++ src/yafowil/widget/cron/example.py
@@ -19,10 +19,18 @@ Cron input.
 def cron_example():
     # Minute Hour DayOfMonth Month DayOfWeek Year
     value = '0,10,20,30,40,50 0,6,12,18 1,15,30 3,6,9,12 1,3,5 2017,2018,2019'
-    cron = factory('cron', name='cronwidget', value=value)
-
+    part = factory(u'fieldset', name='yafowil.widget.cron')
+    part['cron'] = factory(
+        'cron', # '#field:cron'
+        value=value,
+        props={
+            'label': 'Cron Widget',
+            'lang': 'en',
+            'start_year': '2000',
+            'end_year': '2099'
+        })
     return {
-        'widget': cron,
+        'widget': part,
         'doc': DOC_CRON,
         'title': 'Cron',
     }
