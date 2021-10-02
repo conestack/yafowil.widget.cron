735a884cffebc6f779e1abbbcc4f3f325f2e6303
Robert Niederreiter <office@squarewave.at>
2021-10-02 12:55:31 +0200

    Basic ES6 refactoring

--- js/src/bundle.js
+++ js/src/bundle.js
@@ -1,1 +1,14 @@
 import $ from 'jquery';
+
+import {CronWidget} from './widget.js';
+
+export * from './i18n.js';
+export * from './widget.js';
+
+$(function() {
+    if (window.ts !== undefined) {
+        ts.ajax.register(CronWidget.initialize, true);
+    } else {
+        CronWidget.initialize();
+    }
+});
