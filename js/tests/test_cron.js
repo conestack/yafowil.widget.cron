252073bd524a2d273d21352eb492737951aeb819
Lena Daxenbichler <lena.daxenbichler@r2solutions.at>
2022-05-02 13:24:16 +0200

    rudimentary test setup

--- js/tests/test_cron.js
+++ js/tests/test_cron.js
@@ -1,3 +1,10 @@
-QUnit.test('test', assert => {
-    assert.ok(true);
+import { CronWidget } from "../src/widget";
+
+QUnit.test('initialize', assert => {
+    let el = $('<div />').addClass('crontab').addClass('widget');
+    let widget = new CronWidget(el, 'edit');
+    assert.ok(widget);
+
+    el.remove();
+    widget = null;
 });
