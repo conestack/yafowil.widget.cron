0cb8e839944a7f45b730d67988dec90c8f200714
Robert Niederreiter <rnix@squarewave.at>
2022-05-20 16:29:20 +0200

    "Register resources with webresource"

--- src/yafowil/widget/cron/__init__.py
+++ src/yafowil/widget/cron/__init__.py
@@ -1,23 +1,62 @@
 from yafowil.base import factory
 from yafowil.utils import entry_point
 import os
+import webresource as wr
 
 
-resourcedir = os.path.join(os.path.dirname(__file__), 'resources')
+resources_dir = os.path.join(os.path.dirname(__file__), 'resources')
+
+
+##############################################################################
+# Default
+##############################################################################
+
+# webresource ################################################################
+
+scripts = wr.ResourceGroup(name='scripts')
+scripts.add(wr.ScriptResource(
+    name='yafowil-cron-js',
+    depends='jquery-js',
+    directory=resources_dir,
+    resource='widget.js',
+    compressed='widget.min.js'
+))
+
+styles = wr.ResourceGroup(name='styles')
+styles.add(wr.StyleResource(
+    name='yafowil-cron-css',
+    directory=resources_dir,
+    resource='widget.css'
+))
+
+resources = wr.ResourceGroup(name='cron-resources')
+resources.add(scripts)
+resources.add(styles)
+
+# B/C resources ##############################################################
+
 js = [{
     'group': 'yafowil.widget.cron.common',
     'resource': 'widget.js',
     'order': 21,
 }]
-default_css = [{
+css = [{
     'group': 'yafowil.widget.cron.common',
     'resource': 'widget.css',
     'order': 21,
 }]
 
 
+##############################################################################
+# Registration
+##############################################################################
+
 @entry_point(order=10)
 def register():
     from yafowil.widget.cron import widget  # noqa
-    factory.register_theme('default', 'yafowil.widget.cron',
-                           resourcedir, js=js, css=default_css)
+
+    # Default
+    factory.register_theme(
+        'default', 'yafowil.widget.cron', resources_dir,
+        js=js, css=css, resources=resources
+    )
